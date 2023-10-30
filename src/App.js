c97d5bf53d4a317b5add82e4135d85d36543dd8c
Ünal Parlak <unparlak@gmail.com>
2023-10-30 13:34:14 +0300

    form for jobs created, localstorage implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,87 @@
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 function App() {
+  const [storedList, setStoredList] = useState([]);
+
+  const [job, setJob] = useState({
+    id: nanoid(),
+    title: "",
+    company: "",
+    salary: "",
+    applied: false,
+  });
+
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    setJob((prevJob) => {
+      return {
+        ...prevJob,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  }
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(storedList));
+  }, [storedList]);
+
+  function reset() {
+    setJob({
+      id: nanoid(),
+      title: "",
+      company: "",
+      salary: "",
+      applied: false,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(job);
+    setStoredList([...storedList, job]);
+    reset();
+  }
+
   return (
-    <div>
-      <p>buraya bakarlar</p>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Title:
+        <input
+          type="text"
+          name="title"
+          onChange={handleChange}
+          value={job.title}
+        />
+      </label>
+      <label>
+        Company:
+        <input
+          type="text"
+          name="company"
+          onChange={handleChange}
+          value={job.company}
+        />
+      </label>
+      <label>
+        Salary:
+        <input
+          type="text"
+          name="salary"
+          onChange={handleChange}
+          value={job.salary}
+        />
+      </label>
+      <label>
+        Applied:
+        <input
+          type="checkbox"
+          name="applied"
+          onChange={handleChange}
+          checked={job.applied}
+        />
+      </label>
+      <button>Submit</button>
+    </form>
   );
 }
 
