0e655a9e6e4daeb7a4366c060ef583ecccad7d7e
Ünal Parlak <unparlak@gmail.com>
2023-12-15 16:43:22 +0300

    useEffect added for renaming key and values for excel file

--- src/Excel.js
+++ src/Excel.js
@@ -1,9 +1,25 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Context } from "./Context";
 import * as XLSX from "xlsx";
 
 function Excel() {
   const { list } = useContext(Context);
+  const tempList = JSON.parse(localStorage.getItem("items"));
+
+  // To adapt the keys and values of the object to the Excel file
+  useEffect(() => {
+    if (tempList) {
+      tempList.forEach((row) => {
+        // gereksiz olabilir burası
+        // let tempCurrency = row.currency;
+        row.salary = `${row.salary}${row.currency}`;
+        delete row.currency;
+        delete row.id;
+        row.applied = row.applied ? "applied" : "not applied yet";
+        console.log(tempList);
+      });
+    }
+  }, [list, tempList]);
 
   function downloadExcel(data) {
     const worksheet = XLSX.utils.json_to_sheet(data);
@@ -12,7 +28,7 @@ function Excel() {
     XLSX.writeFile(workbook, "DataSheet.xlsx");
   }
 
-  return <button onClick={() => downloadExcel(list)}> Download</button>;
+  return <button onClick={() => downloadExcel(tempList)}> Download</button>;
 }
 
 export default Excel;
